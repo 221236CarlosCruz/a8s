854f3bb46f3b2dd0a56da63f3413e8c47350282b
221236CarlosCruz <221236@ids.upchiapas.edu.mx>
2023-08-13 23:17:28 -0600

    [ADD a3]

--- src/pages/Pruebas.jsx
+++ src/pages/Pruebas.jsx
@@ -1,9 +1,11 @@
 import Atom2 from "../componentes/atomos/Atom2";
+import Atom3 from "../componentes/atomos/Atom3";
 
 function Pruebas() {
     return (
         <>
             <Atom2></Atom2>
+            <Atom3></Atom3>
         </>
     );
 }
