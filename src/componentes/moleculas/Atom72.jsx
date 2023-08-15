a047dc079d009a7f81ba535ceaf8439f45b067ba
221236CarlosCruz <221236@ids.upchiapas.edu.mx>
2023-08-15 01:18:10 -0600

    [UPDATE] component instalaciones2

--- src/componentes/moleculas/Atom72.jsx
+++ src/componentes/moleculas/Atom72.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import image1 from '../../assets/img/ok1.jpg';
 import image2 from '../../assets/img/ok2.jpg';
 import image3 from '../../assets/img/ok3.jpg';
+import image4 from '../../assets/img/ok3.jpg';
+import image5 from '../../assets/img/ok1.jpg';
+import image6 from '../../assets/img/ok2.jpg';
+import image7 from '../../assets/img/ok3.jpg';
+import image8 from '../../assets/img/ok3.jpg';
 
 function Atom72() {
     const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
-    const images = [image1, image2, image3];
+    const images = [image1, image2, image3, image4, image5, image6, image7, image8];
 
     const handleImageChange = (index) => {
         setSelectedImageIndex(index);
@@ -15,24 +20,25 @@ function Atom72() {
     return (
         <div className="container mt-5">
             <div className="row">
-                <div className="col-md-3 d-flex align-items-center justify-content-center">
-                    <ul className="list-group">
-                        {images.map((image, index) => (
-                            <li
-                                key={index}
-                                className={`list-group-item ${selectedImageIndex === index ? 'active' : ''}`}
-                                onClick={() => handleImageChange(index)}
-                            >
-                                Imagen {index + 1}
-                            </li>
-                        ))}
-                    </ul>
+                <div className="col-md-3 position-relative">
+                    <div className="d-flex flex-column align-items-center justify-content-center position-absolute top-0 start-0 w-100 h-100">
+                        <ul className="list-group">
+                            {images.map((image, index) => (
+                                <li
+                                    key={index}
+                                    className={`list-group-item ${selectedImageIndex === index ? 'active' : ''}`}
+                                    onClick={() => handleImageChange(index)}
+                                >
+                                    Imagen {index + 1}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
                 <div className="col-md-9">
                     <div className="text-center">
                         <img src={images[selectedImageIndex]} alt={`Imagen ${selectedImageIndex + 1}`} className="img-fluid" />
                     </div>
-                    <p className="mt-3">Descripción o contenido relacionado a la imagen seleccionada.</p>
                 </div>
             </div>
         </div>
